54313c0b028acc2adcad986ba20a8d499cf177c7
Gavin Daly <gavin@integro.co>
2017-07-05 17:58:30 -0400

    added link for manifest

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<link rel="manifest" href="/wp-content/themes/gavdaly/manifest.json">
+
 <?php wp_head(); ?>
 </head>
 
