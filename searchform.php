a170d874f410f53d70fdbbe0c403af1e332e7ed6
Gavin Daly <gavin@integro.co>
2017-07-05 14:56:05 -0400

    removed span

--- searchform.php
+++ searchform.php
@@ -4,5 +4,5 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentysixteen' ); ?></span>
 		<input type="search" class="search-field" aria-label="search" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'gavdaly' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'gavdaly' ); ?></span></button>
+	 <button type="submit" class="search-submit"><!--<span class="screen-reader-text">--><?php echo _x( 'Search', 'submit button', 'gavdaly' ); ?><!--</span>--></button> 
 </form>
