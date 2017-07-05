14ad716cdaedd1fae5978359e30d4846c7b17742
Gavin Daly <gavin@integro.co>
2017-07-05 14:54:01 -0400

    added search form

---
+++ searchform.php
@@ -0,0 +1,8 @@
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentysixteen' ); ?></span>
+		<input type="search" class="search-field" aria-label="search" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'gavdaly' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	</label>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'gavdaly' ); ?></span></button>
+</form>
